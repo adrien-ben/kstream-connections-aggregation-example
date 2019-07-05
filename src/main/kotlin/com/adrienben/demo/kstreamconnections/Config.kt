e7822d07a613632130a2bc0bd6cd556aaacc5f42
Adrien Bennadji <adrien.bennadji@live.fr>
2019-07-05 14:50:14 +0200

    Refactor

--- src/main/kotlin/com/adrienben/demo/kstreamconnections/Config.kt
+++ src/main/kotlin/com/adrienben/demo/kstreamconnections/Config.kt
@@ -34,47 +34,71 @@ class StreamConfig @Autowired constructor(
     private val logger = LoggerFactory.getLogger(javaClass)
 
     private val stringSerde = Serdes.String()
+    private val stringConsumed = Consumed.with(stringSerde, stringSerde)
     private val serverSerde = jsonSerde(Server::class.java)
+    private val serverProduced = Produced.with(stringSerde, serverSerde)
     private val connectionSerde = jsonSerde(Connection::class.java)
+    private val connectionConsumed = Consumed.with(stringSerde, connectionSerde)
+    private val connectionProduced = Produced.with(stringSerde, connectionSerde)
 
     @Bean
     fun kStream(streamBuilder: StreamsBuilder): KStream<String, Server> {
-        val dnsTable = streamBuilder.table<String, String>(
-                DNS_TOPIC,
-                Consumed.with(stringSerde, stringSerde),
+
+        // Here we consumed the topic containing the hostname to ip mapping as a KTable.
+        val dnsTable = streamBuilder.table(DNS_TOPIC, stringConsumed,
                 materializedAsPersistentStore(DNS_STORE, stringSerde, stringSerde))
 
+        // Here we consumed the connection topic and split it in 3 streams:
+        // - The first contains the connection by ip address
+        // - The second contains the connection by hostname
+        // - The last contains the invalid connections
+        // The keys of these messages are ignored.
         val connections = streamBuilder
-                .stream<String, Connection>(CONNECTIONS_TOPIC, Consumed.with(stringSerde, connectionSerde))
+                .stream(CONNECTIONS_TOPIC, connectionConsumed)
                 .branch(
                         Predicate { _, c -> c.serverIp != null },
                         Predicate { _, c -> c.serverIp == null && c.serverName != null },
                         Predicate { _, c -> c.serverIp == null && c.serverName == null }
                 )
 
-        // Connection with an ip
+        // Connections by ip.
+        // These are just re-keyed and sent to a topic that will contain
+        // all connections with the server ip as their key.
         connections[0]
                 .selectKey { _, v -> v.serverIp }
-                .to(CONNECTIONS_BY_IP_REKEY_TOPIC, Produced.with(stringSerde, connectionSerde))
+                .to(CONNECTIONS_BY_IP_REKEY_TOPIC, connectionProduced)
 
-        // Connection with no Ip but a name. Ip must be resolved.
+        // Connection with no Ip but a name.
+        // Server ip must be resolved by name. It's done by:
+        // - Re-keying the message by server name
+        // - Sending them through a topic to ensure the messages are properly partitioned by server name
+        // - Then we join the re-keyed message to the dns KTable and inject the server name in the connection message
+        // - Then message is re-keyed again (this time by server ip) and sent to the same topic as messages from the previous branch
         connections[1]
                 .selectKey { _, v -> v.serverName }
-                .through(CONNECTIONS_BY_NAME_REKEY_TOPIC, Produced.with(stringSerde, connectionSerde))
+                // The through operation is not mandatory here, just the fact that we combine selectKey and a join operation
+                // would trigger a re-keying of the message. The downside of not being explicit here is that the
+                // topic name would be generated and might change as topology evolves.
+                .through(CONNECTIONS_BY_NAME_REKEY_TOPIC, connectionProduced)
                 .leftJoin(
                         dnsTable,
                         { c, mapping -> c.apply { this.serverIp = mapping } },
                         Joined.with(stringSerde, connectionSerde, stringSerde)
                 )
                 .selectKey { _, v -> v.serverIp }
-                .to(CONNECTIONS_BY_IP_REKEY_TOPIC, Produced.with(stringSerde, connectionSerde))
+                .to(CONNECTIONS_BY_IP_REKEY_TOPIC, connectionProduced)
 
-        // Invalid connections (no ip nor name)
+        // Invalid connections (no ip nor name).
+        // We just log and error for each of them.
         connections[2]
                 .peek { k, v -> logger.error("Received invalid connections: $k - $v") }
 
+        // Now we consume the connections by server ip topic and aggregate the connections events by server:
+        // - First we group all messages by key.
+        // - Then we aggregate them by setting the correct ip and adding a ConnectionEvent in the aggregated message.
+        // - Finally we produce these aggregated in a final topic.
         return streamBuilder
-                .stream<String, Connection>(CONNECTIONS_BY_IP_REKEY_TOPIC, Consumed.with(stringSerde, connectionSerde))
+                .stream(CONNECTIONS_BY_IP_REKEY_TOPIC, connectionConsumed)
                 .groupByKey()
                 .aggregate(
                         { Server() },
@@ -87,12 +111,9 @@ class StreamConfig @Autowired constructor(
                         materializedAsPersistentStore(SITES_AGGREGATIONS_STORE, stringSerde, serverSerde)
                 )
                 .toStream()
-                .apply { this.to(SITES_TOPIC, Produced.with(stringSerde, serverSerde)) }
+                .apply { this.to(SITES_TOPIC, serverProduced) }
     }
 
-    /**
-     * Create a new json Serde for the given [clazz].
-     */
     private fun <T> jsonSerde(clazz: Class<T>) =
             Serdes.serdeFrom(JsonSerializer(mapper), JsonDeserializer(clazz, mapper, false))
 
